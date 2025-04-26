b154149349c719c469647bb69c8aa1b5415e6306
Hayrettin Calban <hayrettin2002@hotmail.com>
2025-04-26 14:13:24 +0300

    son
    3

--- Sifu2D/Assets/Scripts/EnemyHealth.cs
+++ Sifu2D/Assets/Scripts/EnemyHealth.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 3;
     public int currentHealth;
 
+    public Image healthBarFill; // Inspector'dan atayacağın world space health bar'ın fill objesi
+
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
     public void TakeDamage(int damage)
     {
         Debug.Log(gameObject.name + " TakeDamage çağrıldı. Hasar: " + damage + " | Önceki Can: " + currentHealth); // DEBUG
         currentHealth -= damage;
+        UpdateHealthBar();
 
         if (currentHealth <= 0)
         {
             Die();
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = (float)currentHealth / maxHealth;
         }
     }
 
