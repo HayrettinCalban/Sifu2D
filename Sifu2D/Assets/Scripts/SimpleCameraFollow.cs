b0a93b9ea9060542cb1a3ba8a8b593d56d863258
Hayrettin Calban <hayrettin2002@hotmail.com>
2025-04-26 18:33:54 +0300

    son
    7

--- Sifu2D/Assets/Scripts/SimpleCameraFollow.cs
+++ Sifu2D/Assets/Scripts/SimpleCameraFollow.cs
@@ -8,7 +8,7 @@ public class SimpleCameraFollow : MonoBehaviour
     {
         if (target != null)
         {
-            Vector3 newPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            Vector3 newPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
             transform.position = newPosition;
         }
     }
